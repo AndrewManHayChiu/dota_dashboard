1e1d811784b12526c49c94b43e09f5cadd6e6994
AndrewManHayChiu <amhchiu@gmail.com>
2021-08-13 09:38:01 +1000

    update data

--- src/call_data.py
+++ src/call_data.py
@@ -5,32 +5,32 @@ import numpy as np
 
 player_ids = pd.read_csv('../data/player_ids.csv')
 
-api_key = '?api_key=' + key
+# api_key = '?api_key=' + key
 
-end_path = 'players/208812212/'
+# end_path = 'players/208812212/'
 
-url = host_name + end_path + api_key
+# url = host_name + end_path + api_key
 
-url
+# url
 
-response = requests.get(url)
+# response = requests.get(url)
 
-response
+# response
 
-response.text
+# response.text
 
-pretty_json = json.loads(response.text)
-print(json.dumps(pretty_json, indent=4))
+# pretty_json = json.loads(response.text)
+# print(json.dumps(pretty_json, indent=4))
 
-url = host_name + end_path + 'matches/' + api_key
+# url = host_name + end_path + 'matches/' + api_key
 
-r = requests.get(url)
-matches_data = json.loads(r.text)
+# r = requests.get(url)
+# matches_data = json.loads(r.text)
 
-df = pd.DataFrame(matches_data)
-df
+# df = pd.DataFrame(matches_data)
+# df
 
-df.to_csv('data/data.csv')
+# df.to_csv('data/data.csv')
 
 
 
@@ -52,7 +52,7 @@ def get_player_matches(account_id, api_key=api_key):
         
     url = host_name \
         + 'players/' \
-        + account_id \
+        + str(account_id) \
         + '/matches/' \
         + '?api_key=' \
         + api_key \
@@ -107,12 +107,13 @@ match.data
 
 # Extract data for each player
 # matches = get_player_matches(account_id='208812212') # mh
-matches = get_player_matches(account_id='156306162') # shiri
+# matches = get_player_matches(account_id='156306162') # shiri
 # matches = get_player_matches(account_id='1075655293') # bacon
 # matches = get_player_matches(account_id='152471066') # mo
 # matches = get_player_matches(account_id='1075592541') # bottle
 # matches = get_player_matches(account_id='125430576') # king
-# matches = get_player_matches(account_id='103619307') # deathcat
+matches = get_player_matches(account_id='103619307') # deathcat
+# matches = get_player_matches(account_id='100501459') # boss
 
 # This merges data together
 (
@@ -123,8 +124,10 @@ matches = get_player_matches(account_id='156306162') # shiri
         right_on='id'
         )
     .rename(columns={'localized_name': 'hero'})
+    .sort_values(['start_time'])
     .assign(team=lambda x: np.where(x['player_slot'] <= 5, 'radiant', 'dire'),
             win=lambda x: np.where(x['team'] == 'radiant', x['radiant_win'], x['radiant_win'] == False))
+    .assign(avg_win_rate_20=lambda x: x['win'].rolling(window=20).mean())
     .drop(columns=[
         'id', 
         'hero_id', 
@@ -135,14 +138,57 @@ matches = get_player_matches(account_id='156306162') # shiri
         'player_slot', # used to merge other match data
         'radiant_win', 'version'
         ])
-    # .to_csv('./data/matches_mh.csv', index=False)
-    # .to_csv('./data/matches_shiri.csv', index=False)
-    # .to_csv('./data/matches_bacon.csv', index=False)
-    # .to_csv('./data/matches_mo.csv', index=False)
-    # .to_csv('./data/matches_bottle.csv', index=False)
-    # .to_csv('./data/matches_king.csv', index=False)
-    # .to_csv('./data/matches_deathcat.csv', index=False)
 )
+
+def get_data(account_id):
+    matches = get_player_matches(account_id)
+    
+    matches = (
+        pd.DataFrame(matches)
+        .merge(
+            pd.DataFrame(heroes)[['id', 'localized_name']], 
+            left_on='hero_id', 
+            right_on='id'
+            )
+        .rename(columns={'localized_name': 'hero'})
+        .sort_values(['start_time'])
+        .assign(team=lambda x: np.where(x['player_slot'] <= 5, 'radiant', 'dire'),
+                win=lambda x: np.where(x['team'] == 'radiant', x['radiant_win'], x['radiant_win'] == False))
+        .assign(avg_win_rate_20=lambda x: x['win'].rolling(window=20).mean(),
+                avg_kills_20=lambda x: x['kills'].rolling(window=20).mean(),
+                avg_deaths_20=lambda x: x['deaths'].rolling(window=20).mean(),
+                avg_assists_20=lambda x: x['assists'].rolling(window=20).mean())
+        .drop(columns=[
+            'id', 
+            'hero_id', 
+            'game_mode', 
+            'lobby_type', 
+            'leaver_status', 
+            'skill',
+            'player_slot', # used to merge other match data
+            'radiant_win', 'version'
+            ])
+    )
+    
+    return(matches)
+
+# test
+# matches_df = get_data(account_ids['id'][0])
+
+def get_and_save_data(account_id, alias):
+    
+    matches_df = get_data(account_id)
+
+    location = '../data/matches_' + alias + '.csv'
+    matches_df.to_csv(location, index=False)
+
+# test
+# get_and_save_data(account_id=account_ids['id'][0], alias=account_ids['name'][0])
+
+# Get and save data for all account ids
+for i in range(account_ids.shape[0]):
+    get_and_save_data(account_ids['id'][i], account_ids['name'][i])
+
 
 def lane_role(lane_role):
     if (lane_role == 0): return("unknown")
@@ -157,9 +203,9 @@ if __name__ == '__main__':
     
     print('Executing as standalone script')
     
-    ids = pd.read_csv('data/player_ids.csv')
+    account_ids = pd.read_csv('../data/account_ids.csv')
     
-    api_key = open('keys.txt', 'r').read()
+    api_key = open('../keys.txt', 'r').read()
     
     host_name = 'https://api.opendota.com/api/'
     

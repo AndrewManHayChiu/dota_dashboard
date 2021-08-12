3c1dc4c3ac9545792b58bb48fc7dc2b650b094fc
AndrewManHayChiu <amhchiu@gmail.com>
2021-08-12 16:58:36 +1000

    Restrict match call to all draft

--- src/call_data.py
+++ src/call_data.py
@@ -2,8 +2,6 @@ import requests
 import json
 import pandas as pd
 
-host_name = 'https://api.opendota.com/api/'
-
 api_key = '?api_key=' + key
 
 end_path = 'players/208812212/'
@@ -35,39 +33,46 @@ df.to_csv('data/data.csv')
 
 
 # Heroes
-url = host_name + 'heroes'
-heroes = requests.get(url)
-heroes = json.loads(heroes.text)
-pd.DataFrame(heroes)
+def get_heroes():
+    url = host_name + 'heroes'
+    r = requests.get(url)
+    heroes = json.loads(r.text)
+    return(heroes)
 
+# Test
 
-def get_matches(account_id):
+def get_matches(account_id, api_key = api_key):
     
-    url = host_name + 'players/' + account_id + '/matches/' + api_key
+    # Only want normal all-pick matches:
+    # 1: game_mode_all_pick
+    # 22: game_mode_all_draft <- Most matches are 22 for some reason
     
-    response = requests.get(url)
-    data = json.loads(response.text)
-    df = pd.DataFrame(data)
+    url = host_name \
+        + 'players/' \
+        + account_id \
+        + '/matches/' \
+        + '?api_key=' + api_key \
+        + '?game_mode=22'
     
-    return(df)
-
-df = get_matches(account_id='208812212')
-df_new = get_matches(account_id='208812212')
-
-df.shape
-df_new.shape
-
-df
-df_new
-
-def update_data():
+    r = requests.get(url)
+    data = json.loads(r.text)
     
+    return(data)
 
-# def save_data(file_name):
+# Test
+matches = get_matches(account_id='208812212')
+len(matches)
+matches[0]
+matches[-1]
     
 
 
-if __name__ == '__main__':
+if (__name__ == '__main__'):
+    
+    print('Executing as standalone script')
+    
+    import requests
+    import json
     
     ids = pd.read_csv('data/player_ids.csv')
     
